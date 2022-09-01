07ae0f8b6c53a719ce91086a90020da21cc61437
Marcos Felipe <mfelipesp@gmail.com>
2022-09-01 20:44:12 -0300

    Initial Commit

---
+++ PinchUI/PinchUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchUIApp.swift
+//  PinchUI
+//
+//  Created by marcos.felipe.souza on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
